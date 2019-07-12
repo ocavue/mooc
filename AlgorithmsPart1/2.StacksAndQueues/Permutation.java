e121d437c85990a964525c8fa73c50959c0c9361
ocavue <ocavue@gmail.com>
2019-07-13 07:26:24 +0800

    2019-07-13_07-26-24

--- AlgorithmsPart1/2.StacksAndQueues/Permutation.java
+++ AlgorithmsPart1/2.StacksAndQueues/Permutation.java
@@ -1,14 +1,29 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Permutation {
+    // https://en.wikipedia.org/wiki/Reservoir_sampling
     public static void main(String[] args) {
         int k = new Integer(args[0]);
+        int i = 0;
 
         RandomizedQueue<String> q = new RandomizedQueue<String>();
         while (!StdIn.isEmpty()) {
             String s = StdIn.readString();
-            q.enqueue(s);
+
+            i++;
+
+            if (i <= k) {
+                q.enqueue(s);
+            } else {
+                boolean keepNewItem = StdRandom.uniform() < (k / i);
+                if (keepNewItem) {
+                    q.dequeue();
+                    q.enqueue(s);
+                }
+            }
+
         }
         for (int i = 0; i < k; i++) {
             StdOut.println(q.dequeue());
