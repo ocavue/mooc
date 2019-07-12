302cb6b8dc7936c42d792c9869b8124512338ff7
ocavue <ocavue@gmail.com>
2019-07-13 06:35:35 +0800

    2019-07-13_06-35-35

--- AlgorithmsPart1/2.StacksAndQueues/Permutation.java
+++ AlgorithmsPart1/2.StacksAndQueues/Permutation.java
@@ -3,7 +3,7 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Permutation {
     public static void main(String[] args) {
-        StdOut.println(args);
+        StdOut.println(args[0]);
         while (!StdIn.isEmpty()) {
             String s = StdIn.readString();
             StdOut.println(s);
