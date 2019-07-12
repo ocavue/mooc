f897af307595b7d253ede28d513f8d7542bc8518
ocavue <ocavue@gmail.com>
2019-07-13 06:32:08 +0800

    2019-07-13_06-32-07

--- AlgorithmsPart1/2.StacksAndQueues/zip.sh
+++ AlgorithmsPart1/2.StacksAndQueues/zip.sh
@@ -3,7 +3,7 @@ cd `dirname $0`
 mkdir -p ./output
 javac-algs4 Permutation.java
 java-algs4 -ea Permutation 3 < distinct.txt
-java-algs4 -ea Permutation 8 < distinct.txt
+java-algs4 -ea Permutation 8 < duplicates.txt
 zip output/${time}.result.zip ./*.java
 git add --all
 git commit --allow-empty -m ${time}
