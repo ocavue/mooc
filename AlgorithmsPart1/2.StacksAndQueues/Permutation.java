80e1afe3821fe5cc4ac1074c8d418b245d7f0f54
ocavue <ocavue@gmail.com>
2019-07-13 06:31:04 +0800

    2019-07-13_06-31-03

--- AlgorithmsPart1/2.StacksAndQueues/Permutation.java
+++ AlgorithmsPart1/2.StacksAndQueues/Permutation.java
@@ -1,4 +1,11 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Permutation {
     public static void main(String[] args) {
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            StdOut.println(s);
+        }
     }
 }
--- AlgorithmsPart1/2.StacksAndQueues/zip.sh
+++ AlgorithmsPart1/2.StacksAndQueues/zip.sh
@@ -1,5 +1,8 @@
 time=$(date +%Y-%m-%d_%H-%M-%S)
 mkdir -p ./output
+javac-algs4 Permutation.java
+java-algs4 -ea Permutation 3 < distinct.txt
+java-algs4 -ea Permutation 8 < distinct.txt
 zip output/${time}.result.zip ./*.java
 git add --all
 git commit --allow-empty -m ${time}
