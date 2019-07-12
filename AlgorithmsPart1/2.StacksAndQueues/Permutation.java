8d29e3ffea09b76cafa943fff40b764c11733915
ocavue <ocavue@gmail.com>
2019-07-13 07:49:51 +0800

    fix compiling error

--- AlgorithmsPart1/2.StacksAndQueues/Permutation.java
+++ AlgorithmsPart1/2.StacksAndQueues/Permutation.java
@@ -5,7 +5,7 @@ import edu.princeton.cs.algs4.StdRandom;
 public class Permutation {
     // https://en.wikipedia.org/wiki/Reservoir_sampling
     public static void main(String[] args) {
-        Integer k = new Integer.parseInt(args[0]); // https://stackoverflow.com/a/47095501
+        Integer k = Integer.parseInt(args[0]); // https://stackoverflow.com/a/47095501
         int i = 0;
 
         RandomizedQueue<String> q = new RandomizedQueue<String>();
