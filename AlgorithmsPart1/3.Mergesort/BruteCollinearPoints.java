263813f741f738a2e9809e5a9b965d65d5bea744
ocavue <ocavue@gmail.com>
2019-07-21 06:03:48 +0800

    2019-07-21_06-03-48

--- AlgorithmsPart1/3.Mergesort/BruteCollinearPoints.java
+++ AlgorithmsPart1/3.Mergesort/BruteCollinearPoints.java
@@ -23,7 +23,7 @@ public class BruteCollinearPoints {
                         double slopeTo2 = testPoints[0].slopeTo(testPoints[2]);
                         double slopeTo3 = testPoints[0].slopeTo(testPoints[3]);
                         if (slopeTo1 == slopeTo2 && slopeTo2 == slopeTo3) {
-                            StdOut.println("xxxxxx");
+                            StdOut.printf("xxxxxx %d %d %d %d \n", a, b, c, d);
                             addSegment(testPoints[0], testPoints[3]);
                         }
                     }
