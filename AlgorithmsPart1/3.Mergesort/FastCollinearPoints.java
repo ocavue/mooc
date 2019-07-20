2d2699ee27f58d0bfdc06826a7b65b511836aadf
ocavue <ocavue@gmail.com>
2019-07-21 06:37:32 +0800

    2019-07-21_06-37-32

--- AlgorithmsPart1/3.Mergesort/FastCollinearPoints.java
+++ AlgorithmsPart1/3.Mergesort/FastCollinearPoints.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
@@ -5,6 +7,39 @@ import edu.princeton.cs.algs4.StdOut;
 public class FastCollinearPoints {
     public FastCollinearPoints(Point[] points) {
         // finds all line segments containing 4 or more points
+        checkPoints(points);
+        for (int i = 0; i + 3 < points.length; i++) {
+            Point root = points[i];
+
+            Point[] rest = new Point[points.length - i - 1];
+            for (int j = i + 1; j < points.length; j++) {
+                rest[j - i - 1] = points[j];
+            }
+
+            Arrays.sort(rest, root.slopeOrder());
+        }
+    }
+
+    private void checkPoints(Point[] points) {
+        if (points == null)
+            throw new IllegalArgumentException();
+
+        Arrays.sort(points);
+        for (Point p : points) {
+            if (p == null)
+                throw new IllegalArgumentException();
+        }
+        for (int i = 1; i < points.length; i++) {
+            if (points[i - 1] == points[i])
+                throw new IllegalArgumentException();
+        }
+    };
+
+    private boolean isCollinear(Point a, Point b, Point c, Point d) {
+        double sb = a.slopeTo(b);
+        double sc = a.slopeTo(c);
+        double sd = a.slopeTo(d);
+        return (sb == sc && sc == sd);
     }
 
     public int numberOfSegments() {
--- AlgorithmsPart1/3.Mergesort/build.sh
+++ AlgorithmsPart1/3.Mergesort/build.sh
@@ -4,8 +4,8 @@ cd $(dirname $0)
 javac-algs4 ./*.java
 java-algs4 -ea Point
 java-algs4 -ea BruteCollinearPoints input8.txt
-# java-algs4 -ea FastCollinearPoints  input6.txt
-# java-algs4 -ea FastCollinearPoints  input8.txt
+java-algs4 -ea FastCollinearPoints  input6.txt
+java-algs4 -ea FastCollinearPoints  input8.txt
 
 mkdir -p ./output
 time=$(date +%Y-%m-%d_%H-%M-%S)
