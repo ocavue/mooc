9e103883937f8bd5826b80f8d77bb8479f3c7cca
ocavue <ocavue@gmail.com>
2019-07-21 07:17:22 +0800

    2019-07-21_07-17-22

--- AlgorithmsPart1/3.Mergesort/FastCollinearPoints.java
+++ AlgorithmsPart1/3.Mergesort/FastCollinearPoints.java
@@ -5,6 +5,8 @@ import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
 public class FastCollinearPoints {
+    private Point[][] segmentArgs = new Point[0][2];
+
     public FastCollinearPoints(Point[] points) {
         // finds all line segments containing 4 or more points
         checkPoints(points);
@@ -17,6 +19,19 @@ public class FastCollinearPoints {
             }
 
             Arrays.sort(rest, root.slopeOrder());
+            int start = 0;
+            int end = 0;
+            while (end < rest.length) {
+                if ( end + 1 < rest.length && (root.slopeTo(rest[end + 1])) == root.slopeTo(rest[start]) ) {
+                    end = end + 1;
+                } else {
+                    if ((start - end) >= 3) {
+                        addSegment(root, rest[start]);
+                    }
+                    start = end + 1;
+                    end = start;
+                }
+            }
         }
     }
 
@@ -41,6 +56,52 @@ public class FastCollinearPoints {
         double sd = a.slopeTo(d);
         return (sb == sc && sc == sd);
     }
+
+    private void addSegment(Point root, Point[] points) {
+        Point[] newPoints = new Point[points.length + 1];
+        for (int i = 0; i < points.length; i++) {
+            newPoints[i] = points[i];
+        }
+        newPoints[points.length] = root;
+        addSegment(newPoints);
+    }
+
+    private void addSegment(Point[] points) {
+        assert points.length >= 1;
+        Arrays.sort(points);
+        Point start = points[0];
+        Point end = points[points.length - 1];
+        addSegment(start, end);
+    }
+
+    private void addSegment(Point a, Point b) {
+        assert a.compareTo(b) < 0;
+        for (Point[] pair : segmentArgs) {
+            assert pair.length == 2;
+            assert pair[0].compareTo(pair[1]) < 0;
+            if (pair[0].slopeTo(pair[1]) == a.slopeTo(b))
+                break;
+            if (pair[1].compareTo(b) == 0)
+                break;
+        }
+        Point[][] newSegmentArgs = new Point[2][segmentArgs.length + 1];
+        for (int i = 0; i < segmentArgs.length; i++) {
+            newSegmentArgs[i] = segmentArgs[i];
+        }
+        newSegmentArgs[segmentArgs.length][0] = a;
+        newSegmentArgs[segmentArgs.length][1] = b;
+        segmentArgs = newSegmentArgs;
+    }
+
+    // private void addSegment(Point p, Point q) {
+    // LineSegment segment = new LineSegment(p, q);
+
+    // LineSegment[] newSegments = new LineSegment[segments.length + 1];
+    // for (int i = 0; i < segments.length; i++)
+    // newSegments[i] = segments[i];
+    // newSegments[segments.length] = segment;
+    // this.segments = newSegments;
+    // }
 
     public int numberOfSegments() {
         // the number of line segments
@@ -69,7 +130,7 @@ public class FastCollinearPoints {
         // StdDraw.setXscale(0, 32768);
         // StdDraw.setYscale(0, 32768);
         // for (Point p : points) {
-        //     p.draw();
+        // p.draw();
         // }
         // StdDraw.show();
 
