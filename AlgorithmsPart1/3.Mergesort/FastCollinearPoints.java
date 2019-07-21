f734876f98f4d3ce1a6f381e38b7c8de8d43bcb6
ocavue <ocavue@gmail.com>
2019-07-21 17:49:57 +0800

    2019-07-21_17-49-57

--- AlgorithmsPart1/3.Mergesort/FastCollinearPoints.java
+++ AlgorithmsPart1/3.Mergesort/FastCollinearPoints.java
@@ -121,37 +121,37 @@ public class FastCollinearPoints {
 
     public static void main(String[] args) {
 
-        // // read the n points from a file
-        // In in = new In(args[0]);
-        // int n = in.readInt();
-        // Point[] points = new Point[n];
-        // for (int i = 0; i < n; i++) {
-        // int x = in.readInt();
-        // int y = in.readInt();
-        // points[i] = new Point(x, y);
-        // }
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+        int x = in.readInt();
+        int y = in.readInt();
+        points[i] = new Point(x, y);
+        }
 
-        // // draw the points
-        // StdDraw.enableDoubleBuffering();
-        // StdDraw.setXscale(0, 32768);
-        // StdDraw.setYscale(0, 32768);
-        // for (Point p : points) {
-        // p.draw();
-        // }
-        // StdDraw.show();
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+        p.draw();
+        }
+        StdDraw.show();
 
-        // // print and draw the line segments
-        // FastCollinearPoints collinear = new FastCollinearPoints(points);
-        // for (LineSegment segment : collinear.segments()) {
-        // StdOut.println(segment);
-        // segment.draw();
-        // }
-        // StdDraw.show();
+        // print and draw the line segments
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+        StdOut.println(segment);
+        segment.draw();
+        }
+        StdDraw.show();
 
-        Point[] points = new Point[] { new Point(0, 0), new Point(1, 1), new Point(2, 2), new Point(3, 3),
+        points = new Point[] { new Point(0, 0), new Point(1, 1), new Point(2, 2), new Point(3, 3),
                 new Point(4, 4), new Point(1, -1), new Point(2, -2), new Point(3, -3), new Point(4, -4), };
 
-        FastCollinearPoints collinear = new FastCollinearPoints(points);
+        collinear = new FastCollinearPoints(points);
         for (LineSegment segment : collinear.segments()) {
             StdOut.println(segment);
         }
