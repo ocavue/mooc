d85f08479be059eb1b51e40cb2510b2190f7a433
ocavue <ocavue@gmail.com>
2019-07-28 18:34:40 +0800

    test

--- AlgorithmsPart1/4.8Puzzle/Solver.java
+++ AlgorithmsPart1/4.8Puzzle/Solver.java
@@ -2,6 +2,7 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.MinPQ;
 import java.util.Comparator;
+import java.util.Iterator;
 
 public class Solver {
     private MinPQ<SearchNode> thisMQ;
@@ -59,7 +60,7 @@ public class Solver {
 
             this.board = board;
             this.move = move;
-            this.previous = SearchNode;
+            this.previous = previous;
         }
 
     }
@@ -114,7 +115,7 @@ public class Solver {
             }
             boards = new Board[length];
 
-            index = length - 1;
+            int index = length - 1;
             while (node.previous != null) {
                 boards[index] = node.board;
                 node = node.previous;
@@ -135,26 +136,26 @@ public class Solver {
 
     // test client (see below)
     public static void main(String[] args) {
-        // // create initial board from file
-        // In in = new In(args[0]);
-        // int n = in.readInt();
-        // int[][] tiles = new int[n][n];
-        // for (int i = 0; i < n; i++)
-        // for (int j = 0; j < n; j++)
-        // tiles[i][j] = in.readInt();
-        // Board initial = new Board(tiles);
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+        tiles[i][j] = in.readInt();
+        Board initial = new Board(tiles);
 
-        // // solve the puzzle
-        // Solver solver = new Solver(initial);
+        // solve the puzzle
+        Solver solver = new Solver(initial);
 
-        // // print solution to standard output
-        // if (!solver.isSolvable())
-        // StdOut.println("No solution possible");
-        // else {
-        // StdOut.println("Minimum number of moves = " + solver.moves());
-        // for (Board board : solver.solution())
-        // StdOut.println(board);
-        // }
+        // print solution to standard output
+        if (!solver.isSolvable())
+        StdOut.println("No solution possible");
+        else {
+        StdOut.println("Minimum number of moves = " + solver.moves());
+        for (Board board : solver.solution())
+        StdOut.println(board);
+        }
     }
 
 }
--- AlgorithmsPart1/4.8Puzzle/build.sh
+++ AlgorithmsPart1/4.8Puzzle/build.sh
@@ -3,6 +3,8 @@ cd $(dirname $0)
 
 javac-algs4 ./*.java
 java-algs4 Board
+java-algs4 Solver puzzle04.txt
+java-algs4 Solver puzzle3x3-unsolvable.txt
 
 mkdir -p ./output
 time=$(date +%Y-%m-%d_%H-%M-%S)
