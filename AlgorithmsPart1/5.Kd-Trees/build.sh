15566fb552f32df383f1edb80afeed5214feb662
ocavue <ocavue@gmail.com>
2019-08-04 18:03:16 +0800

    add files

---
+++ AlgorithmsPart1/5.Kd-Trees/build.sh
@@ -0,0 +1,14 @@
+set -ex
+cd $(dirname $0)
+
+javac-algs4 ./*.java
+
+java-algs4 KdTreeVisualizer
+java-algs4 NearestNeighborVisualizer ./input100K.txt
+java-algs4 RangeSearchVisualizer ./input100K.txt
+
+mkdir -p ./output
+time=$(date +%Y-%m-%d_%H-%M-%S)
+zip output/${time}.result.zip ./PointSET.java ./KdTree.java
+git add --all
+git commit --allow-empty -m ${time}
