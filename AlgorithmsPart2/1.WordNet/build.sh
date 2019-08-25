395494c8b53d05398831436d07c32fa2ca57fe62
ocavue <ocavue@gmail.com>
2019-08-25 22:21:25 +0800

    2019-08-25_22-21-25

--- AlgorithmsPart2/1.WordNet/build.sh
+++ AlgorithmsPart2/1.WordNet/build.sh
@@ -7,8 +7,8 @@ clear
 javac-algs4 -nowarn ./*.java
 clear
 
-# java-algs4 -ea WordNet
-# java-algs4 -ea SAP digraph1.txt
+java-algs4 -ea WordNet
+java-algs4 -ea SAP digraph1.txt
 java-algs4 -ea SAP digraph2.txt
 java-algs4 -ea Outcast synsets.txt hypernyms.txt outcast5.txt outcast8.txt outcast11.txt
 
