30fedfa8b011dc372bbe76a66be894ba3842681f
ocavue <ocavue@gmail.com>
2019-08-25 17:47:16 +0800

    update

--- AlgorithmsPart2/1.WordNet/WordNet.java
+++ AlgorithmsPart2/1.WordNet/WordNet.java
@@ -1,6 +1,13 @@
+// TODO:
+// Throw a java.lang.IllegalArgumentException in the following situations:
+//      The input to the constructor does not correspond to a rooted DAG.
+//      Any of the noun arguments in distance() or sap() is not a WordNet noun.
+
 public class WordNet {
 
     // constructor takes the name of the two input files
+    // time: linearithmic
+    // space: linear
     public WordNet(String synsets, String hypernyms) {
         if (synsets == null || hypernyms == null)
             throw new IllegalArgumentException();
@@ -11,12 +18,14 @@ public class WordNet {
     }
 
     // is the word a WordNet noun?
+    // time: logarithmic in the number of nouns
     public boolean isNoun(String word) {
         if (word == null)
             throw new IllegalArgumentException();
     }
 
     // distance between nounA and nounB (defined below)
+    // time: linear in the size of the WordNet digraph
     public int distance(String nounA, String nounB) {
         if (nounA == null || nounB == null)
             throw new IllegalArgumentException();
@@ -24,6 +33,7 @@ public class WordNet {
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA
     // and nounB in a shortest ancestral path (defined below)
+    // time: linear in the size of the WordNet digraph
     public String sap(String nounA, String nounB) {
         if (nounA == null || nounB == null)
             throw new IllegalArgumentException();
