48f410d2906730ef33f84174b024155632487c01
ocavue <ocavue@gmail.com>
2019-08-31 19:44:27 +0800

    fix

--- AlgorithmsPart2/2.SeamCarver/SeamCarver.java
+++ AlgorithmsPart2/2.SeamCarver/SeamCarver.java
@@ -40,6 +40,9 @@ public class SeamCarver {
 
    // energy of pixel at column x and row y
    public double energy(int x, int y) {
+      if (x < 0 || x >= width() || y < 0 || y >= height())
+         throw new IllegalArgumentException();
+
       int index = digraphIndex(x, y);
       if (energies[index] == -1) {
          energies[index] = getEnergy(x, y);
@@ -48,9 +51,6 @@ public class SeamCarver {
    }
 
    private double getEnergy(int x, int y) {
-      if (x < 0 || x >= width() || y < 0 || y >= height())
-         throw new IllegalArgumentException();
-
       if (x == 0 || x == width() - 1 || y == 0 || y == height() - 1)
          return 1000;
 
