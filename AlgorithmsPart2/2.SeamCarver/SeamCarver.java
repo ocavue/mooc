c58a85b6356ed722f955c5de6318926471dfe572
ocavue <ocavue@gmail.com>
2019-09-01 18:05:46 +0800

    2019-09-01_18-05-46

--- AlgorithmsPart2/2.SeamCarver/SeamCarver.java
+++ AlgorithmsPart2/2.SeamCarver/SeamCarver.java
@@ -106,8 +106,8 @@ public class SeamCarver {
 
             for (int i = 0; i < fromXs.length; i++) {
                int fromX = fromXs[i];
-               if (distTo[x][y] >= distTo[fromX][fromY] + energy(x, y) + energy(fromX, fromY)) {
-                  distTo[x][y] = distTo[fromX][fromY] + energy(x, y) + energy(fromX, fromY);
+               if (distTo[x][y] >= distTo[fromX][fromY] + energy(x, y)) {
+                  distTo[x][y] = distTo[fromX][fromY] + energy(x, y);
                   pathTo[x][y] = fromX;
                }
             }
@@ -186,8 +186,8 @@ public class SeamCarver {
 
             for (int i = 0; i < fromYs.length; i++) {
                int fromY = fromYs[i];
-               if (distTo[x][y] >= distTo[fromX][fromY] + energy(x, y) + energy(fromX, fromY)) {
-                  distTo[x][y] = distTo[fromX][fromY] + energy(x, y) + energy(fromX, fromY);
+               if (distTo[x][y] >= distTo[fromX][fromY] + energy(x, y)) {
+                  distTo[x][y] = distTo[fromX][fromY] + energy(x, y);
                   pathTo[x][y] = fromY;
                }
             }
