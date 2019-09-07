f2740382a92dd0e18d63c5743949996af050813e
ocavue <ocavue@gmail.com>
2019-09-07 19:17:38 +0800

    update

--- AlgorithmsPart2/3.BaseballElimination/BaseballElimination.java
+++ AlgorithmsPart2/3.BaseballElimination/BaseballElimination.java
@@ -1,4 +1,5 @@
 import edu.princeton.cs.algs4.FordFulkerson;
+import edu.princeton.cs.algs4.FlowNetwork;
 import edu.princeton.cs.algs4.FlowEdge;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
@@ -8,6 +9,8 @@ class BaseballElimination {
     private String[] teams;
     private int[] w, l, r;
     private int[][] g;
+    private int n, s, t;
+    private FlowNetwork network;
 
     public BaseballElimination(String filename) {
         // create a baseball division from given filename in format specified below
@@ -15,13 +18,12 @@ class BaseballElimination {
             throw new IllegalArgumentException();
 
         In in = new In(filename);
-        int n = in.readInt();
+        this.n = in.readInt();
         this.teams = new String[n];
         this.w = new int[n];
         this.l = new int[n];
         this.r = new int[n];
         this.g = new int[n][n];
-
         for (int i = 0; i < n; i++) {
             teams[i] = in.readString();
             w[i] = in.readInt();
@@ -31,11 +33,44 @@ class BaseballElimination {
                 this.g[i][j] = in.readInt();
             }
         }
+
+        // Number of vertices = s and t + team vertices + game vertices
+        // .................. = 2 + n + C(2,n)
+        int vertices = 2 + n + n * (n - 1) / 2;
+        s = vertices - 2;
+        t = vertices - 1;
+
+        this.network = new FlowNetwork(vertices);
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                // connect vertice s to the game vertice
+                // network.addEdge(s);
+            }
+        }
+    }
+
+    private int gameVertice(int team1, int team2) {
+        // TODO BAD MATH
+        assert 0 <= team1 && team1 < team2 && team2 < n;
+        int result = n;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (team1 == i && team2 == j)
+                    return result;
+                result++;
+            }
+        }
+        return -1;
+    }
+
+    private int teamVertice(int team) {
+        assert 0 <= team && team < n;
+        return team;
     }
 
     public int numberOfTeams() {
         // number of teams
-        return this.teams.length;
+        return this.n;
     }
 
     public Iterable<String> teams() {
@@ -90,7 +125,36 @@ class BaseballElimination {
     // }
 
     public static void main(String[] args) {
-        BaseballElimination division = new BaseballElimination(args[0]);
+        String filename = args[0];
+        BaseballElimination division = new BaseballElimination(filename);
+        if (division.n == 4) {
+            assert division.teamVertice(0) == 0;
+            assert division.teamVertice(3) == 3;
+            assert division.gameVertice(0, 1) == 4;
+            assert division.gameVertice(0, 2) == 5;
+            assert division.gameVertice(0, 3) == 6;
+            assert division.gameVertice(1, 2) == 7;
+            assert division.gameVertice(1, 3) == 8;
+            assert division.gameVertice(2, 3) == 9;
+            assert division.s == 10;
+            assert division.t == 11;
+        }
+        if (division.n == 5) {
+            assert division.teamVertice(0) == 0;
+            assert division.teamVertice(4) == 4;
+            assert division.gameVertice(0, 1) == 5;
+            assert division.gameVertice(0, 2) == 6;
+            assert division.gameVertice(0, 3) == 7;
+            assert division.gameVertice(0, 4) == 8;
+            assert division.gameVertice(1, 2) == 9;
+            assert division.gameVertice(1, 3) == 10;
+            assert division.gameVertice(1, 4) == 11;
+            assert division.gameVertice(2, 3) == 12;
+            assert division.gameVertice(2, 4) == 13;
+            assert division.gameVertice(3, 4) == 14;
+            assert division.s == 15;
+            assert division.t == 16;
+        }
         for (String team : division.teams()) {
             // if (division.isEliminated(team)) {
             // StdOut.print(team + " is eliminated by the subset R = { ");
